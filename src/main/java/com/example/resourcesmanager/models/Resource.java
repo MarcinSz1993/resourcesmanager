533db0e6b40876cb3e6448b0d81445adabb7302e
MarcinSz1993 <marcinsz1993@hotmail.com>
2023-09-16 15:01:46 +0200

    Initial commit

---
+++ src/main/java/com/example/resourcesmanager/models/Resource.java
@@ -0,0 +1,2 @@
+package com.example.resourcesmanager.models;public class Resource {
+}
---
+++ src/test/java/com/example/resourcesmanager/ResourcesmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.resourcesmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcesmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
